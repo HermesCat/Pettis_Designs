e398732ff4fa993befdca7258b34d7a120de0fd5
John <jppettis@comcast.net>
2019-08-29 00:08:15 -0400

    basic functionality
    Finished a lot of basic functionality. Need to formate and make links

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,7 @@ let resume = true;
 let about = false;
 let portfolio = false;
 
+$(".portfolio-container").hide();
 
 $(".about-click").click(function () {
     if (resume === true) {
@@ -14,6 +15,25 @@ $(".about-click").click(function () {
         $(".animated-block").animate({
             width: "100%"
         });
+        $(".animated-block").animate({
+            width: "50%",
+            left: "50%"
+        });
+        aboutFormat();
+    }
+    if (portfolio === true) {
+        resume = false;
+        portfolio = false;
+        about = true;
+        $(".animated-block").animate({
+            bottom: "55%"
+        }, 150);
+        $(".animated-block").animate({
+            bottom: "25%"
+        }, 150);
+        $(".animated-block").animate({
+            bottom: "0"
+        }, 150);
         $(".animated-block").animate({
             width: "50%",
             left: "50%"
@@ -41,12 +61,31 @@ $(".resume-click").click(function () {
         });
         resumeFormat();
     }
+    if (portfolio === true) {
+        resume = true;
+        portfolio = false;
+        about = false;
+        $(".animated-block").animate({
+            bottom: "55%"
+        }, 150);
+        $(".animated-block").animate({
+            bottom: "25%"
+        }, 150);
+        $(".animated-block").animate({
+            bottom: "0"
+        }, 150);
+        $(".animated-block").animate({
+            width: "50%",
+            right: "50%"
+        });
+        resumeFormat();
+    }
 });
 
 $(".portfolio-click").click(function () {
     if (about === true) {
-        resume = true;
-        portfolio = false;
+        resume = false;
+        portfolio = true;
         about = false;
         $(".animated-block").animate({
             width: "75%",
@@ -66,10 +105,33 @@ $(".portfolio-click").click(function () {
             bottom: "100%"
         });
         $(".showcase").hide();
-        $(".portfolio-container").animate({
-            bottom: "65%"
+        $(".portfolio-container").show(2000);
+        portfolioFormat();
+    }
+    if (resume === true) {
+        resume = false;
+        portfolio = true;
+        about = false;
+        $(".animated-block").animate({
+            width: "75%",
+            right: "25%"
         });
-        resumeFormat();
+        $(".animated-block").animate({
+            width: "100%",
+            right: "0%"
+        });
+        $(".animated-block").animate({
+            bottom: "50%"
+        });
+        $(".animated-block").animate({
+            bottom: "80%"
+        });
+        $(".animated-block").animate({
+            bottom: "100%"
+        });
+        $(".showcase").hide();
+        $(".portfolio-container").show(550);
+        portfolioFormat();
     }
 });
 
@@ -86,9 +148,13 @@ function aboutFormat() {
         $(".main-title").css("color", "black");
         $(".about-click").addClass("active");
         $(".about-click").addClass("secondary-active");
+        $(".portfolio-click").removeClass("active");
+        $(".portfolio-click").removeClass("secondary-active");
         $(".resume-click").removeClass("active");
         $(".resume-click ").removeClass("secondary-active");
+        $(".showcase").show(400);
         $(".active").css("color", "#ffa500");
+        $(".portfolio-container").hide();
     }
 };
 
@@ -104,8 +170,32 @@ function resumeFormat() {
         $(".main-title").css("color", "white");
         $(".resume-click").addClass("active");
         $(".resume-click").addClass("secondary-active");
+        $(".portfolio-click").removeClass("active");
+        $(".portfolio-click").removeClass("secondary-active");
         $(".about-click").removeClass("active");
         $(".about-click").removeClass("secondary-active");
+        $(".showcase").show(400);
+        $(".active").css("color", "#ffa500");
+        $(".portfolio-container").hide();
+    }
+};
+
+function portfolioFormat() {
+    if (portfolio === true) {
+        $("#secondary-nav").css("text-align", "left");
+        $("#secondary-nav").css("margin-left", "40px");
+        $("#secondary-nav").css("color", "white");
+        $(".about-click").css("color", "white");
+        $(".portfolio-click").css("color", "white");
+        $(".nav-link").css("color", "black");
+        $(".highlighter").css("color", "black");
+        $(".main-title").css("color", "white");
+        $(".about-click").removeClass("active");
+        $(".about-click").removeClass("secondary-active");
+        $(".resume-click").removeClass("active");
+        $(".resume-click ").removeClass("secondary-active");
+        $(".portfolio-click").addClass("active");
+        $(".portfolio-click").addClass("secondary-active");
         $(".active").css("color", "#ffa500");
     }
 };
